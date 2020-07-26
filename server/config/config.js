ba18022c054f1ee6db9df9776287cf59c7e08842
rorrosea <rorrosea@gmail.com>
2020-07-26 17:40:12 -0400

    variable de enterno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ if( process.env.NODE_ENV === 'dev'){
     urlBD = 'mongodb://localhost:27017/cafe';
 }else{
     
-    urlBD = 'mongodb+srv://rorrosea:30lfMCqdLGN5oQCF@cluster0.5kybg.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
